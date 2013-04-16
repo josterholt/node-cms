4ddcfc9142c16db195bf5769bb008f310e525bcc
Shroder <shroder@gmail.com>
2013-04-16 06:17:43 -0700

    Adding crossroads and mysql

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+
 app.get('/', function(req, res) {
 	res.send('Testing');
 })
@@ -8,4 +9,17 @@ var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
 	console.log("Listening on " + port);
-})
+});
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: HOSTNAME,
+	user: USERNAME,
+	password: PASSWORD
+});
+connection.connect();
+connection.query('SELECT * FROM articles LIMIT 1', function (err, rows, fields) {
+	if(err) throw err;
+	console.log(rows);
+});
+connection.end();
